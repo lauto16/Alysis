904e98e151436bdd051c8646cb7e34df3f74febb
lauto16 <ltdev658@gmail.com>
2024-04-07 19:30:37 -0300

    Envio de datos desde frontend hasta backend mediante js y respuesta

--- Alysis/Alysis/static/js/index.js
+++ Alysis/Alysis/static/js/index.js
@@ -1,5 +1,86 @@
 const new_data_set_button = document.getElementById('new-data-set-button')
 
-new_data_set_button.addEventListener('click', function(e){
-	console.log('adding data set')
+//new data set form
+const new_data_set_modal_form = document.getElementById('new-data-set-modal-form')
+const add_new_row_button = document.getElementById('add-new-row')
+const send_new_data_set = document.getElementById('send-new-data-set')
+
+
+function getCookie(name) {
+
+	let cookieValue = null;
+	if (document.cookie && document.cookie !== '') {
+		const cookies = document.cookie.split(';');
+		for (let i = 0; i < cookies.length; i++) {
+			const cookie = cookies[i].trim();
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) === (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+
+
+function sendFormData(data) {
+	console.log(data)
+	let requestOptions = {
+
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			"X-CSRFToken": getCookie('csrftoken'),
+		},
+		// send 
+		body: data
+	}
+
+	fetch('./', requestOptions)
+
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error('Network response was not ok.');
+		})
+
+		.then(data => {
+			console.log(data)
+		})
+
+		.catch(error => {
+			console.error('There was a problem with the fetch operation:', error);
+		})
+}
+
+
+function openDataSetModal(data) {
+	//
+}
+
+
+new_data_set_button.addEventListener('click', function(e) {
+	openDataSetModal()
+})
+
+add_new_row_button.addEventListener('click', function(e) {
+	e.preventDefault()
+})
+
+new_data_set_modal_form.addEventListener('submit', function(e) {
+	e.preventDefault()
+	let data = JSON.stringify({
+		action: 'new_data_set',
+
+		//this should later contain a list with all the data 
+		data: '10'
+
+	})
+	sendFormData(data)
+})
+
+send_new_data_set.addEventListener('click', function(e) {
+	//
 })
--- Alysis/Main/views.py
+++ Alysis/Main/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 def index_view(request):
+
+    if request.method == 'POST':
+        received = json.loads(request.body)
+        action = received['action']
+
+        if action == 'new_data_set':
+            print(received['data'])
+
+            # create a class on utils, this class should contain data validations and return a dict to send
+            # through JsonResponse
+
+            response_data = {
+                'success': True
+            }
+
+            return JsonResponse(response_data)
+
     return render(request, 'index.html')
