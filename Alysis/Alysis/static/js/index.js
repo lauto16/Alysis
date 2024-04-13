c1e94b947bc22d9240838e97a65cc055aadb0e40
lauto16 <ltdev658@gmail.com>
2024-04-12 23:45:08 -0300

    new dataset form mejorado

--- Alysis/Alysis/static/js/index.js
+++ Alysis/Alysis/static/js/index.js
@@ -1,10 +1,10 @@
 const new_data_set_button = document.getElementById('new-data-set-button')
 
 //new data set form
+const new_data_set_close_button = document.getElementById('new-data-set-modal-close-x')
 const new_data_set_modal = document.getElementById('new-data-set-modal')
 const new_data_set_modal_form = document.getElementById('new-data-set-modal-form')
 const add_new_row_button = document.getElementById('add-new-row')
-const send_new_data_set = document.getElementById('send-new-data-set')
 
 
 function getCookie(name) {
@@ -62,14 +62,82 @@ function openModal(modal) {
 }
 
 
+function closeModal(modal) {
+	modal.style.display = 'none'
+}
+
+
+function deleteElements(class_name, except_id){
+	class_name = '.' + class_name
+	document.querySelectorAll(class_name).forEach(function(element) {
+		if (!(element.id === except_id)){
+			element.remove()
+		}
+	});
+}
+
+
+function takeLastAfter(after, string){
+    const parts = string.split(after);
+	return parts.length > 1 ? parts[1] : "";
+}
+
+
+function getLastInputNumber(){
+	let max = 0
+
+	document.querySelectorAll('.data-input').forEach(function(element) {
+		let id_number = parseInt(takeLastAfter(after='_', string=element.id))
+		if (id_number > max){
+			max = id_number
+		}
+	});
+
+	return max
+}
+
+
+function addNewRow(){
+	let max = getLastInputNumber() 
+	max++
+
+	let input_name = 'data-' + max.toString()
+	let input_id = 'data-input_' + max.toString() 
+
+	let container = document.getElementById('new-data-append')
+	let new_input = document.createElement('input')
+	
+	new_input.setAttribute('type', 'text')
+	new_input.setAttribute('name', input_name)
+	new_input.setAttribute('id', input_id)
+	new_input.setAttribute('class', 'data-input')
+	new_input.setAttribute('placeholder', 'Data')
+
+	container.appendChild(new_input)
+
+	new_input.focus();
+}
+
+// open modal
 new_data_set_button.addEventListener('click', function(e) {
 	openModal(new_data_set_modal)
 })
 
+// save actual data row
 add_new_row_button.addEventListener('click', function(e) {
 	e.preventDefault()
+	addNewRow()
 })
 
+// save actual data row (by pressing enter)
+add_new_row_button.addEventListener('keyup', function(e) {
+	if (e.key === 'Enter'){
+		e.preventDefault()
+		addNewRow()
+	}
+})
+
+// send the data to the server
 new_data_set_modal_form.addEventListener('submit', function(e) {
 	e.preventDefault()
 	let data = JSON.stringify({
@@ -80,8 +148,14 @@ new_data_set_modal_form.addEventListener('submit', function(e) {
 
 	})
 	sendFormData(data)
+	new_data_set_modal_form.reset()
+
+	deleteElements(class_name='data-input', except_id='data-input_0')
+
+	closeModal(new_data_set_modal)
 })
 
-send_new_data_set.addEventListener('click', function(e) {
-	//
+// close new data set modal
+new_data_set_close_button.addEventListener('click', function(e){
+	closeModal(new_data_set_modal)
 })
